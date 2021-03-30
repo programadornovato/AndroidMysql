3736cd8b97e54808538a62e5e0f8e13a36e24c48
Programador Novato <info@programadornovato.com>
2021-03-30 12:38:55 -0600

    Crear y llenar una tabla. (Curso de Android Studio y Mysql)[12]
    Video:
    https://youtu.be/xLuKZIpQYwI

--- app/src/main/java/com/programadornovato/androidmysql/MainActivity.kt
+++ app/src/main/java/com/programadornovato/androidmysql/MainActivity.kt
@@ -3,8 +3,11 @@ package com.programadornovato.androidmysql
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
+import android.widget.TableLayout
+import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     var txtEmail:EditText?=null
     var txtTelefono:EditText?=null
     var txtPass:EditText?=null
+    var tbUsuarios:TableLayout?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +27,26 @@ class MainActivity : AppCompatActivity() {
         txtEmail=findViewById(R.id.txtEmail)
         txtTelefono=findViewById(R.id.txtTelefono)
         txtPass=findViewById(R.id.txtPass)
+        tbUsuarios=findViewById(R.id.tbUsuarios)
+        tbUsuarios?.removeAllViews()
+        for (i in 0 until 5){
+            val registro=LayoutInflater.from(this).inflate(R.layout.table_row_np,null,false)
+            val colNombre=registro.findViewById<View>(R.id.colNombre) as TextView
+            val colEmail=registro.findViewById<View>(R.id.colEmail) as TextView
+            val colEditar=registro.findViewById<View>(R.id.colEditar)
+            val colBorrar=registro.findViewById<View>(R.id.colBorrar)
+            colNombre.text="Nombre $i"
+            colEmail.text="Email $i"
+            colEditar.id=i
+            colBorrar.id=i
+            tbUsuarios?.addView(registro)
+        }
+    }
+    fun clickTablaEditar(view: View){
+        Toast.makeText(this,view.id.toString(),Toast.LENGTH_LONG).show()
+    }
+    fun clickTablaBorrar(view: View){
+        Toast.makeText(this,view.id.toString(),Toast.LENGTH_LONG).show()
     }
     fun clickBtnInsertar(view:View){
         val url="http://192.168.8.100/android_mysql/insertar.php"
